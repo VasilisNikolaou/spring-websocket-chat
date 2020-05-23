273a6bcd681ebc71ed9ec459694586a8aba041ce
Vasilis Nikolaou <basilisnik16@gmail.com>
2020-05-23 16:03:29 +0300

    initial commit

---
+++ Spring-WebSocket-Chat/src/main/java/websocket/SpringWebSocketChatApplication.java
@@ -0,0 +1,13 @@
+package websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSocketChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebSocketChatApplication.class, args);
+	}
+
+}
