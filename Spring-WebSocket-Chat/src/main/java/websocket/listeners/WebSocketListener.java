62fc78e7d1587cff55720e006fc46c6242db8444
Vasilis Nikolaou <basilisnik16@gmail.com>
2020-06-01 19:35:16 +0300

    Updated listener class

--- Spring-WebSocket-Chat/src/main/java/websocket/listeners/WebSocketListener.java
+++ Spring-WebSocket-Chat/src/main/java/websocket/listeners/WebSocketListener.java
@@ -1,9 +1,12 @@
 package websocket.listeners;
 
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import websocket.storage.LocalStorage;
 
 @Component
 public class WebSocketListener {
@@ -11,7 +14,12 @@ public class WebSocketListener {
 	
 	@EventListener
 	private void handleSessionConnecton(SessionConnectEvent event) {
+		SimpMessageHeaderAccessor headerAccessor = SimpMessageHeaderAccessor.wrap(event.getMessage());
 		
+		String sessionID = headerAccessor.getSessionId();
+		String username = headerAccessor.getNativeHeader("username").get(0);
+		
+		LocalStorage.getInstance().putActiveUser(sessionID, username);
 	}
 	
 	@EventListener
