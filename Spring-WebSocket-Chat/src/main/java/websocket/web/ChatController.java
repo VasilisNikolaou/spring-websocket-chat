bb922cdbbf175cf5576cdbc92c4b664a113c10c2
Vasilis Nikolaou <basilisnik16@gmail.com>
2020-06-10 20:19:50 +0300

    Updated Controller class

--- Spring-WebSocket-Chat/src/main/java/websocket/web/ChatController.java
+++ Spring-WebSocket-Chat/src/main/java/websocket/web/ChatController.java
@@ -1,8 +1,17 @@
 package websocket.web;
 
+import java.util.Collection;
+
+import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.stereotype.Controller;
+
+import websocket.storage.LocalStorage;
 
 @Controller
 public class ChatController {
 
+	@SubscribeMapping("/participants")
+	public Collection<String> getParticipants() {
+		return LocalStorage.getInstance().getActiveUsers();
+	}
 }
